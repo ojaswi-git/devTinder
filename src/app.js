d59efdc72a785b593a3893d96ad4a72aa4eba56f
ojaswi28-git <ojaswiwadhwa@gmail.com>
2026-02-15 19:45:14 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express=require("express");
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("Test route");
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server");
-})
+app.use("/user",(req,res)=>{
+    res.send("HAHAHAHA");
+});
+app.get("/user",(req,res)=>{
+    res.send("GET METHOD");
+});
+app.post("/user",(req,res)=>{
+    res.send("POST METHOD");
+});
+app.delete("/user",(req,res)=>{
+    res.send("DELETE METHOD");
+});
 app.listen(7777,()=>{
     console.log("server is successfully listening on port 7777");
 });
