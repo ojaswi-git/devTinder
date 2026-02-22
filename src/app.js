2b7846ccdd0e61a4131e075d34f8bb9544874ab5
ojaswi28-git <ojaswiwadhwa@gmail.com>
2026-02-22 12:25:02 +0530

    Exploring REST APIs

--- src/app.js
+++ src/app.js
@@ -4,14 +4,9 @@ const app=express();
 const User=require("./models/user");
 
 require('dotenv').config();
-
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-    const user=new User({
-        firstName:"Akshay",
-        lastName:"Saini",
-        emailId:"akshay@gmail.com",
-        password:"akshay123",
-    });
+    const user=new User(req.body);
     try{
         await user.save();
         res.send("User saved successfully");
@@ -19,6 +14,46 @@ app.post("/signup",async(req,res)=>{
         res.status(400).send("Error saving the user:"+err.message);
     }
 });
+//Get user by email
+app.get("/user",async(req,res)=>{
+    const userEmail=req.body.emailId;
+    try{
+        const user=await User.find({emailId:userEmail});
+        res.send(user);
+    } catch(err){
+        res.status(400).send("Error fetching the user:"+err.message);
+    }
+});
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const users=await User.find({});
+        res.send(users);
+    } catch(err){
+        res.status(400).send("Error fetching the users:"+err.message);
+    }
+});
+
+app.delete("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    try{
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch(err){
+        res.status(400).send("Error deleting the user:"+err.message);
+    }
+});
+
+app.patch("/user",async(req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        await User.findByIdAndUpdate(userId,data);
+        res.send("User updated successfully");
+    } catch(err){
+        res.status(400).send("Error updating the user:"+err.message);
+    }
+});    
 connectDB()
 .then(()=>{
     console.log("Database connection established");
